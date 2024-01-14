957baf1b9219dce7144d28749becef9e7ad5283e
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-01-14 11:42:08 +0530

    Update Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { db } from "../config/firebase";
 import { getDocs, query, collection, orderBy, limit } from "firebase/firestore";
 
-const Videos = () => {
+const Videos = ({ videos }) => {
   const [vidlist, setVidlist] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -31,9 +31,6 @@ const Videos = () => {
         setIsLoading(true);
         const videosData = await getVids();
         setVidlist(videosData);
-
-        // Store the fetched data in local storage
-        localStorage.setItem("videosData", JSON.stringify(videosData));
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -41,22 +38,13 @@ const Videos = () => {
       }
     };
 
-    // Check if data is already present in local storage
-    const storedVideosData = JSON.parse(localStorage.getItem("videosData"));
-
-    if (storedVideosData) {
-      // Use stored data if available
-      setVidlist(storedVideosData);
-      setIsLoading(false);
-    } else {
-      // Fetch data if not present in local storage
-      fetchData();
-    }
-  }, []); // Fetch data on component mount
+    // Fetch data on component mount
+    fetchData();
+  }, []);
 
   return (
     <div className="containerv">
-      {vidlist.map((video) => (
+      {videos.map((video) => (
         <div className="video-card" key={video.id}>
           <video className="video" src={video.vidurl} controls></video>
           <div className="video-details">
