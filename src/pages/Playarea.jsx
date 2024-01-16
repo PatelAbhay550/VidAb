d8e52e0e6552a9bf110331503d67ab8db87dd75b
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-01-16 11:09:54 +0530

    Update Playarea.jsx
    views feature

--- src/pages/Playarea.jsx
+++ src/pages/Playarea.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { db } from "../config/firebase";
+import { auth, db } from "../config/firebase";
 import { Link } from "react-router-dom";
 import { CiShare2 } from "react-icons/ci";
-import { doc, getDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  serverTimestamp,
+  collection,
+  addDoc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import styles from "./Playarea.module.css";
 
 const Playarea = () => {
   const { videoId } = useParams();
   const [video, setVideo] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [viewsCount, setViewsCount] = useState(0);
 
   useEffect(() => {
     const fetchVideoDetails = async () => {
@@ -20,6 +30,28 @@ const Playarea = () => {
         if (videoDoc.exists()) {
           setVideo(videoDoc.data());
           document.title = videoDoc.data().title;
+
+          // Check if the user has already viewed this video
+          const existingViewQuery = query(
+            collection(db, "Views"),
+            where("videoId", "==", videoId),
+            where("userId", "==", auth.currentUser?.uid || null)
+          );
+          const existingViewSnapshot = await getDocs(existingViewQuery);
+
+          if (existingViewSnapshot.size === 0) {
+            // User hasn't viewed the video yet, record the view
+            const newViewData = {
+              videoId,
+              userId: auth.currentUser?.uid || null,
+              timestamp: serverTimestamp(),
+            };
+            await addDoc(collection(db, "Views"), newViewData);
+            setViewsCount((prevCount) => prevCount + 1);
+            console.log("View recorded successfully!");
+          } else {
+            setViewsCount(existingViewSnapshot.size); // Set the correct view count
+          }
         } else {
           console.log("Video not found");
         }
@@ -29,7 +61,6 @@ const Playarea = () => {
         setIsLoading(false);
       }
     };
-
     fetchVideoDetails();
   }, [videoId]);
 
@@ -38,7 +69,6 @@ const Playarea = () => {
       navigator
         .share({
           title: video.title,
-
           url: window.location.href,
         })
         .then(() => console.log("Share successful"))
@@ -70,6 +100,7 @@ const Playarea = () => {
               <p>
                 Uploaded: {video.dateupload} {video.timeupload}
               </p>
+              <p>{viewsCount} View(s)</p>
             </div>
           </div>
 
